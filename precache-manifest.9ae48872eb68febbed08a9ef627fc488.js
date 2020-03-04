4d5c4c66704de391b97f13670086ceef852e90d5
John Leon <johnleon@192.168.0.4>
2020-03-04 18:03:12 -0500

    Agregando archivos construidos de la aplicacion

---
+++ precache-manifest.9ae48872eb68febbed08a9ef627fc488.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa206dd38d4451c18d7b1890837d4970",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1de3e39d67819013082d",
+    "url": "/static/css/main.dd55359b.chunk.css"
+  },
+  {
+    "revision": "0558e131d81046928409",
+    "url": "/static/js/2.d7f80620.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.d7f80620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1de3e39d67819013082d",
+    "url": "/static/js/main.c3768096.chunk.js"
+  },
+  {
+    "revision": "72636ba1915a0fdf8b23",
+    "url": "/static/js/runtime-main.db83cb7a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
