050f6d1746c2feaa239b43d05a50e558b7d32165
jleon253 <jleon253@gmail.com>
2020-03-04 19:31:52 -0500

    Ajustando ruta para homepage

---
+++ precache-manifest.3be67dfd7dcb7ec862e0f1c7efd20986.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5437be733bfd0a57f0312e87df7f25e9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1de3e39d67819013082d",
+    "url": "./static/css/main.dd55359b.chunk.css"
+  },
+  {
+    "revision": "0558e131d81046928409",
+    "url": "./static/js/2.d7f80620.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.d7f80620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1de3e39d67819013082d",
+    "url": "./static/js/main.c3768096.chunk.js"
+  },
+  {
+    "revision": "1d5ab0b648e4ed9339b6",
+    "url": "./static/js/runtime-main.4997ee1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9ae48872eb68febbed08a9ef627fc488.js"
+  "./precache-manifest.3be67dfd7dcb7ec862e0f1c7efd20986.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
